fa9a709bd72671db3d21a8b67f02a7452811343d
Tio Rambe Putra <tiorambe1104@gmail.com>
2023-08-18 03:01:54 +0700

    Changed the mechanism. Substract now's work

--- app.js
+++ app.js
@@ -1,108 +1,131 @@
 const buttons = document.querySelector('#buttons');
-const display = document.querySelector('#display');
+const prevCalc = document.querySelector('#prevCalc');
+const curCalc = document.querySelector('#curCalc');
 
 buttons.addEventListener('click', (e) => {
 	const button = e.target;
 	const tool = button.dataset.tool;
 	const buttonContent = button.innerText;
-	const displayContent = display.innerText;
-	const sliced = displayContent.split('');
-	// const slicedPerSpaced = displayContent.split(' ');
-	const calculator = [];
-	const operator = /[%*+/]/;
+	const curText = curCalc.innerText;
+	const prevText = prevCalc.innerText;
+	const lastCurText = curText[curText.length - 1];
+	const lastPrevText = prevText[prevText.length - 1];
+	const operator = /[+*-/]/g;
 
 	if (!tool) {
-		if (displayContent == 0) {
-			display.innerText = buttonContent;
+		if (curText.includes('+')) {
+			prevCalc.innerText = curText;
+			curCalc.innerText = '';
+		}
+		if (curText.includes('*')) {
+			prevCalc.innerText = curText;
+			curCalc.innerText = '';
+		}
+		if (curText.includes('/')) {
+			prevCalc.innerText = curText;
+			curCalc.innerText = '';
+		}
+		if (curText.includes('%')) {
+			prevCalc.innerText = curText;
+			curCalc.innerText = '';
+		}
+		if (curText.includes('-')) {
+			prevCalc.innerText = curText;
+			curCalc.innerText = '';
+		}
+		if (curText == '') {
+			curCalc.innerText = buttonContent;
+		} else if (curText == '0') {
+			curCalc.innerText += `.${buttonContent}`;
 		} else {
-			display.innerText += buttonContent;
+			curCalc.innerText += buttonContent;
 		}
 	}
 
-	if (tool === 'clearAll') {
-		display.innerText = 0;
-	}
-
-	if (tool === 'plusMin') {
-		// if (!/\W$/.test(slicedPerSpaced[slicedPerSpaced.length - 1])) {
-		// 	const minus = '-'.concat(slicedPerSpaced[slicedPerSpaced.length - 1]);
-		// 	slicedPerSpaced.splice(-1, 1, minus);
-		// }
-		// slicedPerSpaced.join(' ');
-	}
-
-	if (tool === 'percent') {
-		if (!operator.test(displayContent)) {
-			display.innerText += buttonContent;
+	if (tool == 'decimal') {
+		if (curText != '' && !curText.includes(buttonContent) && curText != /\w/g) {
+			curCalc.innerText += buttonContent;
 		}
 	}
 
-	if (tool === 'divide') {
-		if (!operator.test(displayContent)) {
-			display.innerText += buttonContent;
+	if (tool == 'add') {
+		if (
+			lastCurText != '.' &&
+			curText != '' &&
+			!curText.includes(buttonContent) &&
+			!prevText.includes(buttonContent)
+		) {
+			curCalc.innerText += buttonContent;
 		}
 	}
 
-	if (tool === 'multiply') {
-		if (!operator.test(displayContent)) {
-			display.innerText += buttonContent;
+	if (tool == 'divide') {
+		if (
+			lastCurText != '.' &&
+			curText != '' &&
+			!curText.includes(buttonContent) &&
+			!prevText.includes(buttonContent)
+		) {
+			curCalc.innerText += buttonContent;
 		}
 	}
 
-	if (tool === 'substract') {
-		if (!operator.test(displayContent)) {
-			display.innerText += buttonContent;
+	if (tool == 'percent') {
+		if (
+			lastCurText != '.' &&
+			curText != '' &&
+			!curText.includes(buttonContent) &&
+			!prevText.includes(buttonContent)
+		) {
+			curCalc.innerText += buttonContent;
 		}
 	}
 
-	if (tool === 'add') {
-		if (!operator.test(displayContent)) {
-			display.innerText += buttonContent;
-			const forCalc = displayContent.split(buttonContent);
-			calculator.push(forCalc);
+	if (tool == 'multiply') {
+		if (
+			lastCurText != '.' &&
+			curText != '' &&
+			!curText.includes(buttonContent) &&
+			!prevText.includes(buttonContent)
+		) {
+			curCalc.innerText += buttonContent;
 		}
 	}
 
-	if (tool === 'clear') {
-		if (/\s$/.test(displayContent)) {
-			sliced.splice(-3, 3);
-		} else {
-			sliced.pop();
+	if (tool == 'substract') {
+		if (
+			lastCurText != '.' &&
+			curText != '' &&
+			lastCurText != '-' &&
+			lastPrevText != '-'
+			// !curText.includes(buttonContent) &&
+			// !prevText.includes(buttonContent)
+		) {
+			curCalc.innerText += buttonContent;
 		}
-		if (displayContent.length <= 1) {
-			display.innerText = 0;
-		}
-
-		display.innerText = sliced.join('');
 	}
 
-	if (tool === 'decimal') {
-		const slicedForDecimal = displayContent.split(' ');
-		if (!/\W/.test(slicedForDecimal[slicedForDecimal.length - 1])) {
-			slicedForDecimal[slicedForDecimal.length - 1] += buttonContent;
+	if (tool == 'equal') {
+		if (prevText.includes('*') && curText != '' && lastCurText != '.') {
+			curCalc.innerText = parseFloat(prevText) * parseFloat(curText);
+			prevCalc.innerText = '';
 		}
-		display.innerText = slicedForDecimal.join(' ');
-	}
-
-	if (tool === 'equal') {
-		let total;
-		if (displayContent.includes('+')) {
-			const calc = displayContent.split('+');
-			total = parseFloat(calc[0]) + parseFloat(calc[1]);
-		} else if (displayContent.includes('*')) {
-			const calc = displayContent.split('*');
-			total = parseFloat(calc[0]) * parseFloat(calc[1]);
-		} else if (displayContent.includes('/')) {
-			const calc = displayContent.split('/');
-			total = parseFloat(calc[0]) / parseFloat(calc[1]);
-		} else if (displayContent.includes('%')) {
-			const calc = displayContent.split('%');
-			total = (parseFloat(calc[1]) / 100) * parseFloat(calc[0]);
-		} else if (displayContent.includes('-')) {
-			const calc = displayContent.split('-');
-			total = parseFloat(calc[0]) - parseFloat(calc[1]);
+		if (prevText.includes('+') && curText != '' && lastCurText != '.') {
+			curCalc.innerText = parseFloat(prevText) + parseFloat(curText);
+			prevCalc.innerText = '';
 		}
-		display.innerText = total;
+		if (prevText.includes('/') && curText != '' && lastCurText != '.') {
+			curCalc.innerText = parseFloat(prevText) / parseFloat(curText);
+			prevCalc.innerText = '';
+		}
+		if (prevText.includes('%') && curText != '' && lastCurText != '.') {
+			curCalc.innerText = parseFloat(prevText) * (parseFloat(curText) / 100);
+			prevCalc.innerText = '';
+		}
+		if (lastPrevText == '-' && curText != '' && lastCurText != '.') {
+			curCalc.innerText = parseFloat(prevText) - parseFloat(curText);
+			prevCalc.innerText = '';
+		}
 	}
 });
 
@@ -110,119 +133,65 @@ buttons.addEventListener('click', (e) => {
 // 	const button = e.target;
 // 	const tool = button.dataset.tool;
 // 	const buttonContent = button.innerText;
-// 	const displayContent = display.innerText;
-// 	const sliced = displayContent.split('');
-// 	const slicedPerSpaced = displayContent.split(' ');
+// 	const curText = curCalc.innerText;
+// 	const prevText = prevCalc.innerText;
+// 	const lastCurText = curText[curText.length - 1];
 
 // 	if (!tool) {
-// 		if (displayContent == 0) {
-// 			display.innerText = buttonContent;
+// 		if (curText.includes('+')) {
+// 			prevCalc.innerText = curText;
+// 			curCalc.innerText = '';
+// 		}
+// 		if (curText.includes('*')) {
+// 			prevCalc.innerText = curText;
+// 			curCalc.innerText = '';
+// 		}
+// 		if (curText == '') {
+// 			curCalc.innerText = buttonContent;
 // 		} else {
-// 			display.innerText += buttonContent;
+// 			curCalc.innerText += buttonContent;
 // 		}
 // 	}
 
-// 	if (tool === 'clearAll') {
-// 		display.innerText = 0;
-// 	}
-
-// 	if (tool === 'plusMin') {
-// 		// if (!/\W$/.test(slicedPerSpaced[slicedPerSpaced.length - 1])) {
-// 		// 	const minus = '-'.concat(slicedPerSpaced[slicedPerSpaced.length - 1]);
-// 		// 	slicedPerSpaced.splice(-1, 1, minus);
-// 		// }
-// 		// slicedPerSpaced.join(' ');
-// 	}
-
-// 	if (tool === 'percent') {
-// 		if (!/\W$/.test(displayContent)) {
-// 			display.innerText += buttonContent;
+// 	if (tool == 'decimal') {
+// 		if (curText != '' && !curText.includes(buttonContent)) {
+// 			curCalc.innerText += buttonContent;
 // 		}
 // 	}
 
-// 	if (tool === 'divide') {
-// 		if (!/\W$/.test(displayContent)) {
-// 			display.innerText += buttonContent;
+// 	if (tool == 'add') {
+// 		if (
+// 			prevText.includes(buttonContent) &&
+// 			curText != '' &&
+// 			lastCurText != '.'
+// 		) {
+// 			curCalc.innerText = parseFloat(prevText) + parseFloat(curText);
+// 			prevCalc.innerText = '';
+// 		}
+// 		if (
+// 			lastCurText != '.' &&
+// 			curText != '' &&
+// 			!curText.includes(buttonContent)
+// 		) {
+// 			curCalc.innerText += buttonContent;
 // 		}
 // 	}
 
-// 	if (tool === 'multiply') {
-// 		if (!/\W$/.test(displayContent)) {
-// 			display.innerText += buttonContent;
+// 	if (tool == 'multiply') {
+// 		if (
+// 			prevText.includes(buttonContent) &&
+// 			curText != '' &&
+// 			lastCurText != '.'
+// 		) {
+// 			curCalc.innerText = parseFloat(prevText) * parseFloat(curText);
+// 			prevCalc.innerText = '';
+// 		}
+// 		if (
+// 			lastCurText != '.' &&
+// 			curText != '' &&
+// 			!curText.includes(buttonContent)
+// 		) {
+// 			curCalc.innerText += buttonContent;
 // 		}
 // 	}
-
-// 	if (tool === 'substract') {
-// 		if (!/\W$/.test(displayContent)) {
-// 			display.innerText += buttonContent;
-// 		}
-// 	}
-
-// 	if (tool === 'add') {
-// 		if (!/\W$/.test(displayContent)) {
-// 			display.innerText += buttonContent;
-// 		}
-// 	}
-
-// if (tool === 'clear') {
-// 	if (/\s$/.test(displayContent)) {
-// 		sliced.splice(-3, 3);
-// 	} else {
-// 		sliced.pop();
-// 	}
-// 	if (displayContent.length <= 1) {
-// 		display.innerText = 0;
-// 	}/\W$/.test
-
-// 	display.innerText = sliced.join('');
-// }
-
-// if (tool === 'decimal') {
-// if (!/\W$/.test(slicedPerSpaced[slicedPerSpaced.length - 1])) {
-// 	const slicedForDecimal = slicedPerSpaced.slice(-1);
-// 	if (
-// 		!slicedForDecimal[0].includes(buttonContent) &&
-// 		slicedForDecimal[0] !== ''
-// 	) {
-// 		slicedForDecimal[0] += buttonContent;
-// 		slicedPerSpaced.splice(-1, 1, slicedForDecimal);
-// 	}
-// 	display.innerText = slicedPerSpaced.join(' ');
-// }
-// }
-
-// 	if (tool === 'equal') {
-// 		display.innerText += buttonContent;
-// 	}
-// });
-
-// if (tool === 'clear') {
-// 	sliced.pop();
-// 	display.innerText = sliced.join('');
-// 	if (displayContent.length <= 1) {
-// 		display.innerText = 0;
-// 	}
-// }
-
-// if (tool === 'equal') {
-// 	let total;
-// 	if (displayContent.includes('+')) {
-// 		const calc = displayContent.split('+');
-// 		total = parseFloat(calc[0]) + parseFloat(calc[1]);
-// 	} else if (/\d-/g.test(displayContent)) {
-// 		// /.\d+-./
-// 		const calc = displayContent.split('-');
-// 		total = parseFloat(calc[0]) - parseFloat(calc[1]);
-// 	} else if (displayContent.includes('*')) {
-// 		const calc = displayContent.split('*');
-// 		total = parseFloat(calc[0]) * parseFloat(calc[1]);
-// 	} else if (displayContent.includes('/')) {
-// 		const calc = displayContent.split('/');
-// 		total = parseFloat(calc[0]) / parseFloat(calc[1]);
-// 	} else if (displayContent.includes('%')) {
-// 		const calc = displayContent.split('%');
-// 		total = (parseFloat(calc[1]) / 100) * parseFloat(calc[0]);
-// 	}
-// 	display.innerText = total;
-// }
 // });
